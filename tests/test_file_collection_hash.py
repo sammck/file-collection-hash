ce3e7694684d1f575647c13f0931abe0a7e8389d
Sam McKelvie <dev@mckelvie.org>
2022-05-23 15:36:59 -0700

    fix test

--- tests/test_file_collection_hash.py
+++ tests/test_file_collection_hash.py
@@ -1,7 +1,7 @@
 from file_collection_hash import __version__ as library_version
 
 # The following is automatically updated by semantic-release
-_file_collection_hash_version = '1.1.0'
+_file_collection_hash_version = '0.1.0'
 
 def test_version():
     assert library_version == _file_collection_hash_version
